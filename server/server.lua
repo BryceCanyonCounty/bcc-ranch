019d62095fdcc8d890a8c6e2fb48ab06b9a4cda5
Jake2k4 <119366047+jakeyboi1@users.noreply.github.com>
2023-05-27 13:10:18 -0500

    Bug Fixes

--- server/server.lua
+++ server/server.lua
@@ -35,31 +35,6 @@ RegisterServerEvent('bcc-ranch:AddItem', function(item, amount)
 end)
 
 ---------------------- DB AREA ----------------------------------
--------- Creates DB ----------
-CreateThread(function()
-    exports.oxmysql:execute([[CREATE TABLE if NOT EXISTS `ranch` (
-        `charidentifier` varchar(50) NOT NULL,
-        `ranchcoords` LONGTEXT NOT NULL,
-        `ranchname` varchar(100) NOT NULL,
-        `ranch_radius_limit` varchar(100) NOT NULL,
-        `ranchid` int NOT NULL AUTO_INCREMENT,
-        `ranchCondition` int(10) NOT NULL DEFAULT 0,
-        `cows` varchar(50) NOT NULL DEFAULT 'false',
-        `cows_cond` int(10) NOT NULL DEFAULT 0,
-        `cows_age` int(10) NOT NULL DEFAULT 0,
-        `pigs` varchar(50) NOT NULL DEFAULT 'false',
-        `pigs_cond` int(10) NOT NULL DEFAULT 0,
-        `pigs_age` int(10) NOT NULL DEFAULT 0,
-        `chickens` varchar(50) NOT NULL DEFAULT 'false',
-        `chickens_cond` int(10) NOT NULL DEFAULT 0,
-        `chickens_age` int(10) NOT NULL DEFAULT 0,
-        `goats` varchar(50) NOT NULL DEFAULT 'false',
-        `goats_cond` int(10) NOT NULL DEFAULT 0,
-        `goats_age` int(10) NOT NULL DEFAULT 0,
-        PRIMARY KEY `ranchid` (`ranchid`),
-        UNIQUE KEY `charidentifier` (`charidentifier`))
-    ]])
-end)
 
 ------ Create Ranch Db Handler -----
 RegisterServerEvent('bcc-ranch:InsertCreatedRanchIntoDB', function(ranchname, ranchradius, ownerstaticid, coords)
