f90b6b5fed244758fa81a9fe4fc025380a58fef0
BurntJannings <82676805+BurntJannings@users.noreply.github.com>
2023-05-28 21:24:09 -0600

    Update MainRanchMenu.lua
    Added Employee System

--- client/menusetup/MainRanchMenu.lua
+++ client/menusetup/MainRanchMenu.lua
@@ -4,11 +4,13 @@ function MainMenu()
     TriggerEvent('bcc-ranch:MenuClose')
     MenuData.CloseAll()
     local elements = {
-        { label = _U("CheckRanchCond"), value = 'checkranchcond', desc = _U("CheckRanchCond_desc") },
-        { label = _U("Caretaking"), value = 'caretaking', desc = _U("Caretaking_desc") },
-        { label = _U("BuyAnimals"), value = 'buyanimals', desc = _U("BuyAnimals_desc") },
-        { label = _U("ManageAnimals"), value = 'manageanimals', desc = _U("ManageAnimals_desc") },
-        { label = _U("Inventory"), value = 'openinv', desc = _U("Inventory_desc") },
+        { label = _U("CheckRanchCond"), value = 'checkranchcond',  desc = _U("CheckRanchCond_desc") },
+        { label = _U("Caretaking"),     value = 'caretaking',      desc = _U("Caretaking_desc") },
+        { label = _U("BuyAnimals"),     value = 'buyanimals',      desc = _U("BuyAnimals_desc") },
+        { label = _U("ManageAnimals"),  value = 'manageanimals',   desc = _U("ManageAnimals_desc") },
+        { label = _U("ManageEmployee"), value = 'manageemployees', desc = _U("ManageEmployee_desc") },
+        { label = _U("Ledger"),         value = 'ledger',          desc = _U("Ledger") },
+        { label = _U("Inventory"),      value = 'openinv',         desc = _U("Inventory_desc") },
     }
 
     MenuData.Open('default', GetCurrentResourceName(), 'menuapi',
@@ -27,6 +29,47 @@ function MainMenu()
                 TriggerServerEvent('bcc-ranch:DisplayRanchCondition', RanchId)
             elseif data.current.value == 'buyanimals' then
                 BuyAnimalMenu()
+            elseif data.current.value == 'manageanimals' then
+                ManageOwnedAnimalsMenu()
+            elseif data.current.value == 'manageemployees' then
+                EmployeeMenu()
+            elseif data.current.value == 'ledger' then
+                Inmenu = false
+                MenuData.CloseAll()
+                TriggerServerEvent('bcc-ranch:GetLedger', RanchId)
+
+                Wait(100)
+            elseif data.current.value == 'openinv' then
+                TriggerServerEvent('bcc-ranch:OpenInv', RanchId)
+            end
+        end)
+end
+
+function MainMenuEmployee()
+    Inmenu = true
+    TriggerEvent('bcc-ranch:MenuClose')
+    MenuData.CloseAll()
+    local elements = {
+        { label = _U("CheckRanchCond"), value = 'checkranchcond', desc = _U("CheckRanchCond_desc") },
+        { label = _U("Caretaking"),     value = 'caretaking',     desc = _U("Caretaking_desc") },
+        { label = _U("ManageAnimals"),  value = 'manageanimals',  desc = _U("ManageAnimals_desc") },
+        { label = _U("Inventory"),      value = 'openinv',        desc = _U("Inventory_desc") },
+    }
+
+    MenuData.Open('default', GetCurrentResourceName(), 'menuapi',
+        {
+            title = _U("RanchMenuName"),
+            align = 'top-left',
+            elements = elements,
+        },
+        function(data)
+            if data.current == 'backup' then
+                _G[data.trigger]()
+            end
+            if data.current.value == 'caretaking' then
+                CareTakingMenu()
+            elseif data.current.value == 'checkranchcond' then
+                TriggerServerEvent('bcc-ranch:DisplayRanchCondition', RanchId)
             elseif data.current.value == 'manageanimals' then
                 ManageOwnedAnimalsMenu()
             elseif data.current.value == 'openinv' then
