b0f59f52d6589f356d8371df6ea49d6cfabb768d
BurntJannings <82676805+BurntJannings@users.noreply.github.com>
2023-05-28 21:23:20 -0600

    Update MainRanch.lua
    Added Employee System

--- client/MainRanch.lua
+++ client/MainRanch.lua
@@ -1,12 +1,16 @@
 InMission = false
+
 ----- This will run to check if the player owns a ranch when they select char -----
-RegisterNetEvent('vorp:SelectedCharacter')
-AddEventHandler('vorp:SelectedCharacter', function()
-    Wait(50)
-    local player = GetPlayerServerId(tonumber(PlayerId())) --credit vorp_admin
-    Wait(200)
-    TriggerServerEvent("bcc-ranch:getPlayersInfo", player) --credit vorp_admin
-    TriggerServerEvent('bcc-ranch:CheckIfRanchIsOwned')
+CreateThread(function()
+    RegisterNetEvent('vorp:SelectedCharacter')
+    AddEventHandler('vorp:SelectedCharacter', function()
+        Wait(50)
+        local player = GetPlayerServerId(tonumber(PlayerId())) --credit vorp_admin
+        Wait(200)
+        TriggerServerEvent("bcc-ranch:getPlayersInfo", player) --credit vorp_admin
+        TriggerServerEvent('bcc-ranch:CheckIfRanchIsOwned')
+        TriggerServerEvent('bcc-ranch:CheckIfInRanch')
+    end)
 end)
 
 CreateThread(function()
@@ -16,6 +20,7 @@ CreateThread(function()
             Wait(200)
             TriggerServerEvent("bcc-ranch:getPlayersInfo", player) --credit vorp_admin
             TriggerServerEvent('bcc-ranch:CheckIfRanchIsOwned')
+            TriggerServerEvent('bcc-ranch:CheckIfInRanch')
         end)
     end
 end)
@@ -27,9 +32,11 @@ RegisterNetEvent('bcc-ranch:HasRanchHandler', function(ranch)
     RanchId = ranch.ranchid
     TriggerEvent('bcc-ranch:StartCondDec')
     TriggerServerEvent('bcc-ranch:AgeCheck', RanchId)
-    local blip = VORPutils.Blips:SetBlip(ranch.ranchname, Config.RanchSetup.BlipHash, 0.2, RanchCoords.x, RanchCoords.y, RanchCoords.z)
-    local  PromptGroup = VORPutils.Prompts:SetupPromptGroup()
-    local firstprompt = PromptGroup:RegisterPrompt(_U("OpenRanchMenu"), 0x760A9C6F, 1, 1, true, 'hold', {timedeventhash = "MEDIUM_TIMED_EVENT"})
+    local blip = VORPutils.Blips:SetBlip(ranch.ranchname, Config.RanchSetup.BlipHash, 0.2, RanchCoords.x, RanchCoords.y,
+        RanchCoords.z)
+    local PromptGroup = VORPutils.Prompts:SetupPromptGroup()
+    local firstprompt = PromptGroup:RegisterPrompt(_U("OpenRanchMenu"), 0x760A9C6F, 1, 1, true, 'hold',
+        { timedeventhash = "MEDIUM_TIMED_EVENT" })
     if Config.RanchSetup.AnimalsRoamRanch then
         TriggerServerEvent('bcc-ranch:WanderingSetup', RanchId)
     end
@@ -41,12 +48,16 @@ RegisterNetEvent('bcc-ranch:HasRanchHandler', function(ranch)
         if dist < 5 then
             PromptGroup:ShowGroup(_U("OpenRanchMenu_desc"))
             if firstprompt:HasCompleted() then
-                if not Inmenu then
-                    if not InMission then
+                if not InMission then
+                    TriggerServerEvent('bcc-ranch:CheckisOwner')
+                    Wait(250)
+                    if IsOwner then
                         MainMenu()
                     else
-                        VORPcore.NotifyRightTip(_U("inmission"), 4000)
+                        MainMenuEmployee()
                     end
+                else
+                    VORPcore.NotifyRightTip(_U("inmission"), 4000)
                 end
             end
         elseif dist > 200 then
@@ -58,7 +69,8 @@ end)
 ---- This Event Will Create The Sale Locations Blips and decrease the ranches cond over time -----
 AddEventHandler('bcc-ranch:StartCondDec', function()
     for k, v in pairs(Config.SaleLocations) do
-        local  blip = VORPutils.Blips:SetBlip(v.LocationName, Config.SaleLocationBlipHash, 0.2, v.Coords.x, v.Coords.y, v.Coords.z)
+        local blip = VORPutils.Blips:SetBlip(v.LocationName, Config.SaleLocationBlipHash, 0.2, v.Coords.x, v.Coords.y,
+            v.Coords.z)
     end
     while true do
         Wait(Config.RanchSetup.RanchCondDecrease)
@@ -78,4 +90,12 @@ end)
 
 RegisterNetEvent('bcc-ranch:Openranchmanagerment', function()
     TriggerServerEvent('bcc-ranch:GetAllRanches')
+end)
+
+RegisterNetEvent('bcc-ranch:IsOwned', function(result)
+    IsOwner = result
+end)
+
+RegisterNetEvent('bcc-ranch:SendList', function(result)
+    table.insert(Employees, result)
 end)
