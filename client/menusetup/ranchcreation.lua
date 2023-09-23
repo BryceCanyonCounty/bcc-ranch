afdb60bf975bfecc7b2e38e5100bd9310d7ae144
BurntJannings <82676805+BurntJannings@users.noreply.github.com>
2023-09-22 21:10:51 -0600

    Update ranchcreation.lua

--- client/menusetup/ranchcreation.lua
+++ client/menusetup/ranchcreation.lua
@@ -8,7 +8,7 @@ RegisterNetEvent('bcc-ranch:CreateRanchmenu', function()
 end)
 
 function CreateRanchMen()
-    local ranchName, ranchRadius
+    local ranchName, ranchRadius,taxes
     local coords = GetEntityCoords(PlayerPedId())
     Inmenu = true
     CreationMenu = true
@@ -17,6 +17,7 @@ function CreateRanchMen()
         { label = _U("StaticId"),         value = 'staticid',    desc = _U("StaticId_desc") },
         { label = _U("NameRanch"),        value = 'nameranch',   desc = _U("NameRanch_desc") },
         { label = _U("RanchRadiusLimit"), value = 'radiuslimit', desc = _U("RanchRadiusLimit_desc") },
+        { label = _U("TaxAmount"),        value = 'taxes',       desc = _U("TaxAmount") },
         { label = _U("Confirm"),          value = 'confirm',     desc = _U("Confirm_desc") }
     }
 
@@ -54,6 +55,30 @@ function CreateRanchMen()
                         if result ~= '' and result then
                             ranchName = result
                             VORPcore.NotifyRightTip(_U("nameSet"), 4000)
+                        else
+                            VORPcore.NotifyRightTip(_U("InvalidInput"), 4000)
+                        end
+                    end)
+                end,
+                    ['taxes'] = function()
+                    local myInput = {
+                        type = "enableinput",                                               -- don't touch
+                        inputType = "textarea",                                             -- input type
+                        button = _U("Confirm"),                                             -- button name
+                        placeholder = _U("TaxAmount"),                                      -- placeholder name
+                        style = "block",                                                    -- don't touch
+                        attributes = {
+                            inputHeader = "",                                               -- header
+                            type = "number",                                                  -- inputype text, number,date,textarea ETC
+                            pattern = "[0-9]",                                          --  only numbers "[0-9]" | for letters only "[A-Za-z]+"
+                            title = _U("InvalidInput"),                                     -- if input doesnt match show this message
+                            style = "border-radius: 10px; background-color: ; border:none;" -- style
+                        }
+                    }
+                    TriggerEvent("vorpinputs:advancedInput", json.encode(myInput), function(result)
+                        if result ~= '' and result then
+                            taxes = result
+                            VORPcore.NotifyRightTip(_U("taxesSet"), 4000)
                         else
                             VORPcore.NotifyRightTip(_U("InvalidInput"), 4000)
                         end
