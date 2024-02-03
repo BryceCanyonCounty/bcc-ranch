f9643f388c6ac196fab7bddf5749e31eab6436bb
Jake2k4 <119366047+jakeyboi1@users.noreply.github.com>
2024-02-03 13:16:44 -0600

    feedingFix (#82)

--- client/services/animalshelper/feedanimals.lua
+++ client/services/animalshelper/feedanimals.lua
@@ -78,7 +78,7 @@ RegisterNetEvent('bcc-ranch:FeedAnimals', function(animalType)
             model = 'a_c_sheep_01'
             spawnCoords = json.decode(RanchData.sheep_coords)
             eatAnim = joaat("WORLD_ANIMAL_SHEEP_EATING_GROUND")
-            if tonumber(RanchData.sheeps_age) < Config.animalSetup.sheeps then
+            if tonumber(RanchData.sheeps_age) < Config.animalSetup.sheeps.AnimalGrownAge then
                 scale = 0.5
             end
         end,
@@ -87,7 +87,7 @@ RegisterNetEvent('bcc-ranch:FeedAnimals', function(animalType)
             model = 'a_c_goat_01'
             spawnCoords = json.decode(RanchData.goat_coords)
             eatAnim = joaat("PROP_ANIMAL_GOAT_EAT_TROUGH")
-            if tonumber(RanchData.goat_coords) < Config.animalSetup.goats then
+            if tonumber(RanchData.goat_coords) < Config.animalSetup.goats.AnimalGrownAge then
                 scale = 0.5
             end
         end,
@@ -96,7 +96,7 @@ RegisterNetEvent('bcc-ranch:FeedAnimals', function(animalType)
             model = 'a_c_chicken_01'
             eatAnim = joaat("WORLD_ANIMAL_CHICKEN_EATING")
             spawnCoords = json.decode(RanchData.chicken_coords)
-            if tonumber(RanchData.chicken_coords) < Config.animalSetup.chickens then
+            if tonumber(RanchData.chicken_coords) < Config.animalSetup.chickens.AnimalGrownAge then
                 scale = 0.5
             end
         end
